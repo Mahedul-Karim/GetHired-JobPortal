4cde766ed39bfeddcedf3838f4e13af3bda1bb0e
Mahedul-Karim <karimrupo21@gmail.com>
2025-01-18 16:27:31 +0600

    hero bug fix

--- frontend/src/components/home/Hero.tsx
+++ frontend/src/components/home/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className={`bg-[url('./assets/hero-bg.png')] bg-no-repeat bg-cover py-36 bg-[top_center]`} >Hero</div>
+    <div className={`bg-[url('/assets/hero-bg.png')] bg-no-repeat bg-cover py-36 bg-[top_center]`} >Hero</div>
   )
 }
 
