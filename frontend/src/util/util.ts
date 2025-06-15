0edae29af7c496023d1120dc919a3d3a2ded94c4
Mahedul-Karim <125360313+Mahedul-Karim@users.noreply.github.com>
2025-06-15 16:01:24 +0600

    Update util.ts

--- frontend/src/util/util.ts
+++ frontend/src/util/util.ts
@@ -1,8 +1,8 @@
 export const ANIMATION_DELAY = 100; /**in milliseconds */
 export const ITEMS_PER_PAGE = 8;
 export const MAX_FILE_UPLOAD_SIZE = 10 * 1024 * 1024; /**in megabyte */
-// export const BACKEND_URL = 'https://get-hired-server.vercel.app/api/v1';
-export const BACKEND_URL = "http://localhost:3000/api/v1";
+export const BACKEND_URL = 'https://get-hired-server.vercel.app/api/v1';
+// export const BACKEND_URL = "http://localhost:3000/api/v1";
 
 export const formatDate = (date: Date) => {
   return date.toLocaleDateString("en-GB", {
